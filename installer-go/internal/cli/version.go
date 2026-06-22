54a238c58c817534708cffd423c65f390df22a14
NovusEdge <novusedge0@gmail.com>
2026-06-22 23:07:48 +0300

    feat(installer): add version command

---
+++ installer-go/internal/cli/version.go
@@ -0,0 +1,25 @@
+// version.go
+package cli
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show version info",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("engrammic %s\n", Version)
+		fmt.Printf("  commit: %s\n", Commit)
+		fmt.Printf("  built:  %s\n", Date)
+		fmt.Printf("  go:     %s\n", runtime.Version())
+		fmt.Printf("  os:     %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
